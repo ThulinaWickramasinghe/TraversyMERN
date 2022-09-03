3a56e2e4ac0bb2c9ebbb2c93748e6882dd01f2e1
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2022-09-03 10:31:34 +0530

    Backend API complete

--- be/routes/goal.routes.js
+++ be/routes/goal.routes.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const {getGoals,setGoal,putGoal,deleteGoal} = require('../controllers/goalController')
+const {protect} = require('../middleware/authMiddleware')
 
 
-router.route('/').get(getGoals).post(setGoal)
+router.route('/').get(protect,getGoals).post(protect,setGoal)
 
 // router.get('/' , getGoals)
 
 // router.post('/' , setGoal)
 
-router.put('/:id' , putGoal)
 
-router.delete('/:id' , deleteGoal)
+router.route('/:id').delete(protect,deleteGoal).put(protect, putGoal)
+
+// router.put('/:id' , putGoal)
+
+// router.delete('/:id' , deleteGoal)
 
 module.exports = router
--- be/server.js
+++ be/server.js
@@ -3,6 +3,8 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
+const goalRoutes = require('./routes/goal.routes')
+const userRoutes = require('./routes/user.routes')
 
 const connectDB = require('./config/db')
 connectDB()
@@ -12,8 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
-const goalRoutes = require('./routes/goal.routes')
 app.use('/api/goals' , goalRoutes)
-
+app.use('/api/users' , userRoutes)
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server has started on port ${port}`))
